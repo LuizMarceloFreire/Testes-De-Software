406317d6654614b58081546260b65300b79b0135
Luiz Marcelo Freire <luiz.marcelo-developer@outlook.com>
2022-09-13 17:32:39 -0300

    Add project files.

---
+++ Demo/Calculadora.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    public class Calculadora
+    {
+        public double Somar(int valor1, int valor2)
+        {
+            return valor1 + valor2;
+        }
+
+        public int Dividir(int valor1, int valor2)
+        {
+            return valor1 / valor2;
+        }
+    }
+}
